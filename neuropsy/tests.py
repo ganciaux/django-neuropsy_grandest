d2bdfe1076b31b17a8fbb0017e271c1b09b3fb85
ganciaux <ganciaux@MBP-de-ganciaux.fritz.box>
2020-10-07 07:51:01 +0200

    Start tests and fixtures

--- neuropsy/tests.py
+++ neuropsy/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from neuropsy.models import Article
 
-# Create your tests here.
+class ArticleTestCase(TestCase):
+    def setUp(self):
+        Article.objects.create(label="Bilan 1", description="Court", amount=460.00)
+        Article.objects.create(label="Bilan 2", description="Long", amount=260.00)
+
+    def test_article_label(self):
+        a1 = Article.objects.get(label="Bilan 1")
+        a2 = Article.objects.get(label="Bilan 2")
+
